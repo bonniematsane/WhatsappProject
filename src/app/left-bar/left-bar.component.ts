6c3e4547c137d887d551720a8644cd204bf0fc84
Bonakele Matsane <Bonakele.Matsane@ekurhuleni.gov.za>
2023-07-18 13:44:14 +0200

    final commit

--- src/app/left-bar/left-bar.component.ts
+++ src/app/left-bar/left-bar.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./left-bar.component.scss']
 })
 export class LeftBarComponent {
-
+  items = [
+    {
+      image: '../../assets/notti.png',
+      title: 'Notifications'
+    },
+    {
+      image: '../../assets/sec.png',
+      title: 'Security'
+    },
+    {
+      image: '../../assets/theme.png',
+      title: 'Theme'
+    },
+    {
+      image: '../../assets/chat.png',
+      title: 'Chatwallpaper'
+    },
+    {
+      image: '../../assets/blocked.jpg',
+      title: 'Blocked'
+    },
+    {
+      image: '../../assets/desktop.jpg',
+      title: 'Desktop settings'
+    },
+    {
+      image: '../../assets/keyb.png',
+      title: 'KeyboardShortcuts'
+    },
+    {
+      image: '../../assets/help.png',
+      title: 'Help'
+    }
+  ];
 }
